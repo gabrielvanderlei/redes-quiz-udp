bffb1edbff641a984a8a527d28afe28a4778f6c0
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-08 13:40:50 -0300

    Feature(quizz): continuos sending message from client to server

--- server.py
+++ server.py
@@ -1,40 +1,34 @@
 import socket
 
-localIP             = "127.0.0.1"
-localPort           = 20001
-bufferSize          = 1024
+ 
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 1024
+msgFromServer       = "Hello UDP Client"
+bytesToSend         = str.encode(msgFromServer)
 
+ 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+ 
 # Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
+
 print("UDP server up and listening")
 
-def sendCommand(clientAddress, command, message):
-    UDPServerSocket.sendto(str.encode('{0} {1}'.format(command, message)), clientAddress)
 
 # Listen for incoming datagrams
-QUIZ_STATE = 'QUIZ_STATE_WAITING_P1'
+while(True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
 
-def runServerCycle():
-    while(True):
-        messageAndClientAddress = UDPServerSocket.recvfrom(bufferSize)
-        message = messageAndClientAddress[0]
-        clientAddress = messageAndClientAddress[1]
+    clientMsg = "Message from Client: {}".format(message)
+    clientIP  = "Client IP Address: {}".format(address)
+    
+    print(clientMsg)
+    print(clientIP)
 
-        commandAndData = message.split(' ')
-        command = message[0]
-        data = message[1:]
-
-        process(clientAddress, command, data)
-
-def process(clientAddress, command, data):
-    if(command == 'init'):
-        sendCommand('message', 'Cliente {0} adicionado ao quiz'.format(clientAddress))
-    if(command == 'message'):
-        sendCommand('message', 'Cliente {0} enviou: {1}'.format(clientAddress, data))
-    elif(command == 'end'):
-        sendCommand('message', 'Cliente desconectado')
-
-runServerCycle()
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
