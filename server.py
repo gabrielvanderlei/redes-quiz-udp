2be9f32be354be3b98d1716fd048a69154f11be5
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-08 15:28:11 -0300

    Feature(quizz): impossible to connect in the server when there's a match running

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ localIP     = "127.0.0.1"
 localPort   = 20001
 bufferSize  = 1024
 
+commands = ['start', 'register']
+quizzStartedFlag = False
 listOfClientsConnected = []
 maxNumberOfClients = 5
 
@@ -16,7 +18,7 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
 
-print("UDP server up and listening")
+print("UDP Server is running at port {}".format(localPort))
 
 
 # Listen for incoming datagrams
@@ -25,19 +27,28 @@ while(True):
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
 
-    # clientMsg = "Message from Client: {}".format(message)
-    # clientIP  = "Client IP Address: {}".format(address)
-    if len(listOfClientsConnected) < maxNumberOfClients:
-        if message == 'register':
-            if not (address in listOfClientsConnected):
-                listOfClientsConnected.append(address)
-                msgFromServer = "Client successfully registred!"
-            else:
-                msgFromServer = "Client already registred!"
-    else:
-        msgFromServer = "The Database is currently full!"
-    # print(clientMsg)
-    # print(clientIP)
+    if not (message in commands):
+        msgFromServer = "Enter a correct Command \n"
+    
+    if (message == 'register' or message == 'start') and (quizzStartedFlag):
+        msgFromServer = "There's a Quizz match running right now! \n"
+
+    if (message == 'start') and not (quizzStartedFlag):
+        msgFromServer = "Quizz started! \n"
+        quizzStartedFlag = True
+
+    if not quizzStartedFlag:
+        if (len(listOfClientsConnected) < maxNumberOfClients):
+            if message == 'register':
+                if not (address in listOfClientsConnected):
+                    listOfClientsConnected.append(address)
+                    msgFromServer = "Client successfully registred! \n"
+                    print('Client {} registred in the Database'.format(address))
+                else:
+                    msgFromServer = "Client already registred! \n"
+        else:
+            msgFromServer = "The Database is currently full! \n"
+
 
     # Sending a reply to client
     bytesToSend         = str.encode(msgFromServer)
