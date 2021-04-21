85e474ce3e3a1f23d887f09b3e5027de5ca1eb03
Gabriel Vanderlei <gabrielvanderlei11@gmail.com>
2021-04-21 01:25:08 +0000

    feat(timer): added timer and improved quiz

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import threading
 import socket
 
-def clientCycle():
-    # Create a UDP socket at client side
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-    # Send to server using created UDP socket
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    
+def readServerDataCycle():
     while True:
-        msgFromClient = input("Command: ")
+        bufferSize = 1024
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        msg = "> {0}".format(msgFromServer[0].decode("utf-8"))
+        print(msg)
+        
+def waitingCommandCycle():
+    print("Send messages writing below and pressing enter.")
+    print("Send 'register' to enter in a server room.")
+    print("A room only support 5 clients.")
+    
+    while True:
+        msgFromClient = input("")
         bytesToSend = str.encode(msgFromClient)
         serverAddressPort = ("127.0.0.1", 20001)
-        bufferSize = 1024
-
         UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-        msg = "Message from Server: \n {0}".format(msgFromServer[0].decode("utf-8"))
-
-        print(msg)
 
 
-clientThread = threading.Thread(target=clientCycle, args=[])
-clientThread.start()
+readServerThread = threading.Thread(target=readServerDataCycle, args=[])
+readServerThread.start()
+
+waitingCommandThread = threading.Thread(target=waitingCommandCycle, args=[])
+waitingCommandThread.start()
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import time
+import collections
 import pprint
 import threading
 import socket
@@ -7,12 +9,21 @@ POINTS_WINNING = +25
 POINTS_LOSING = -5
 POINTS_NONE_ANSWER = -1
 MAX_ROUNDS = 5
+MAX_ROUND_TIME = 10000
 
 localIP = "127.0.0.1"
 localPort = 20001
 bufferSize = 1024
 questions = []
 userPoints = {}
+quizzStartedFlag = False
+atualQuestion = 0
+commands = ['start', 'register']
+listOfClientsConnected = []
+maxNumberOfClients = 5
+address = 0
+waitingResponse = False
+waitingResponseTimer = 0
 
 print("Reading question list.")
 
@@ -29,64 +40,70 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 print("UDP Server is running at port {}".format(localPort))
 
-def serverCycle(): 
-    atualQuestion = 0
-    commands = ['start', 'register']
-    quizzStartedFlag = False
-    listOfClientsConnected = []
-    maxNumberOfClients = 5
-    address = 0
-
-    def getQuestions():
-        print("Loading questions to quiz.")
-        allNotSelectedQuestions = questionList
-        questions = []
-        
-        for questionId in range(0, MAX_ROUNDS):
-            numberOfQuestions = len(allNotSelectedQuestions)
-            questionChoosed = random.randint(0, numberOfQuestions - 1)
-            questions.append(allNotSelectedQuestions.pop(questionChoosed))
-        
-        print("Questions successfully loaded.")
-        return questions
-        
-        
-
-    def sendMessageToClient(msgFromServer):
-        bytesToSend = str.encode(msgFromServer)
-        print('Sending to {0}: {1}'.format(address, msgFromServer))
-        UDPServerSocket.sendto(bytesToSend, address)
-
-    def sendMessageToAllClients(msgFromServer):
-        for addressOfEachClientConected in listOfClientsConnected:
-            address = addressOfEachClientConected
-            sendMessageToClient(msgFromServer)
-
-    def runNextQuestion(questionId, correctMessage=False):
-        if questionId < MAX_ROUNDS:
-            if not correctMessage:
-                sendMessageToAllClients("Question {0}: {1}".format(questionId + 1, questions[questionId][0]))
-            else:
-                sendMessageToAllClients("{0} \n Question {1}: {2}".format(correctMessage, questionId + 1, questions[questionId][0]))
-        else:
-            message = "End of the quiz. \n"
-            message += "The final pontuation was: \n"
-            
-            for userAddress in userPoints:
-                points = userPoints[userAddress]
-                message += "{0}: {1}".format(userAddress, points)
-            
-            sendMessageToAllClients(message)
-            quizzStartedFlag = False
+def getQuestions():
+    print("Loading questions to quiz.")
+    allNotSelectedQuestions = questionList
+    questions = []
     
-        return questionId
+    for questionId in range(0, MAX_ROUNDS):
+        numberOfQuestions = len(allNotSelectedQuestions)
+        questionChoosed = random.randint(0, numberOfQuestions - 1)
+        questions.append(allNotSelectedQuestions.pop(questionChoosed))
+    
+    print("Questions successfully loaded.")
+    return questions
+
+def sendMessageToClient(msgFromServer):
+    bytesToSend = str.encode(msgFromServer)
+    print('Sending to {0}: {1}'.format(address, msgFromServer))
+    UDPServerSocket.sendto(bytesToSend, address)
+
+def sendMessageToAllClients(msgFromServer):
+    for addressOfEachClientConected in listOfClientsConnected:
+        address = addressOfEachClientConected
+        sendMessageToClient(msgFromServer)
+
+def runNextQuestion(questionId, correctMessage=False):
+    global waitingResponse
+    
+    if questionId < MAX_ROUNDS:
+        sendMessageToAllClients("Question {0}: {1}".format(questionId + 1, questions[questionId][0]))
+    else:
+        waitingResponse = False
+        sendMessageToAllClients("End of the quiz.")
+        sendMessageToAllClients("The final pontuation was:")
+        
+        for userAddress in collections.OrderedDict(sorted(userPoints.items())):
+            points = userPoints[userAddress]
+            sendMessageToAllClients("{0}: {1}".format(userAddress, points))
+        
+        quizzStartedFlag = False
+
+    return questionId
+
+def serverCycle(): 
+    global POINTS_WINNING
+    global POINTS_LOSING
+    global MAX_ROUNDS
+    global MAX_ROUND_TIME
+    
+    global bufferSize
+    global questions
+    global userPoints
+    global quizzStartedFlag
+    global atualQuestion
+    global commands
+    global listOfClientsConnected
+    global maxNumberOfClients
+    global address
+    global waitingResponse
+    global waitingResponseTimer
 
     # Listen for incoming datagrams
     while(True):
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
         message = bytesAddressPair[0].decode("utf-8")
         address = bytesAddressPair[1]
-
         print('Received from client: {0} / {1}'.format(message, address))
 
         if not quizzStartedFlag:
@@ -96,34 +113,76 @@ def serverCycle():
             
             # When the user try to join in a current match
             if (message == 'register' or message == 'start') and (quizzStartedFlag):
-                sendMessageToClient( "There's a Quizz match running right now! \n")
+                sendMessageToClient( "There's a Quizz match running right now!")
                 
             if (len(listOfClientsConnected) < maxNumberOfClients):
                 if message == 'register':
                     if not (address in listOfClientsConnected):
                         listOfClientsConnected.append(address)
                         userPoints[address] = 0
-                        sendMessageToClient("Client successfully registred! \n")
+                        sendMessageToClient("Client successfully registered!")
+                        sendMessageToClient("\n")
+                        sendMessageToClient("Wellcome!")
+                        sendMessageToClient("Correct answers will add to your pontuaction will be changed by {0}".format(POINTS_WINNING))
+                        sendMessageToClient("Wrong answers will add to your pontuaction will be changed by  {0}".format(POINTS_LOSING))
+                        sendMessageToClient("If any correct response has been sent in 10 seconds, all players will have the pontuaction changed by {0}".format(POINTS_NONE_ANSWER))
+                        sendMessageToClient("Message 'start' to close room and begin the quiz")
+                        sendMessageToClient("\n")
                         print('Client {} registred in the Database'.format(address))
                     else:
-                        sendMessageToClient("Client already registred! \n")
+                        sendMessageToClient("Client already registred!")
             else:
-                sendMessageToClient("The Database is currently full! \n")
+                sendMessageToClient("The Database is currently full!")
             
             if (message == 'start'):
+                waitingResponse = True
+                waitingResponseTimer = int(round(time.time() * 1000))
                 questions = getQuestions()
                 atualQuestion = runNextQuestion(atualQuestion)
                 quizzStartedFlag = True
 
         else:
             if questions[atualQuestion][1] == message:
+                waitingResponse = True
+                waitingResponseTimer = int(round(time.time() * 1000))
                 userPoints[address] += POINTS_WINNING
-                atualQuestion = runNextQuestion(atualQuestion + 1, "Correct! {0} has the right answer: {1}".format(address, message))
+                sendMessageToAllClients("Correct! {0} has the right answer: {1}".format(address, message))
+                sendMessageToClient("Pontuaction changed: {0}. New pontuation: {1}".format(POINTS_WINNING, userPoints[address]))
+                atualQuestion = runNextQuestion(atualQuestion + 1)
             else:
                 userPoints[address] += POINTS_LOSING
                 sendMessageToClient("Wrong... Try again")
+                sendMessageToClient("Pontuaction changed: {0}. New pontuation: {1}".format(POINTS_LOSING, userPoints[address]))
 
-
+def timerCycle():
+    global POINTS_NONE_ANSWER
+    
+    global userPoints
+    global quizzStartedFlag
+    global atualQuestion
+    global address
+    global waitingResponse
+    global waitingResponseTimer
+    
+    while True:        
+        atualMilliseconds = int(round(time.time() * 1000))
+        pastMilliseconds = (atualMilliseconds - waitingResponseTimer)
+        
+        if waitingResponse and (pastMilliseconds > MAX_ROUND_TIME):
+            waitingResponse = False
+            
+            for addressOfEachClientConected in listOfClientsConnected:
+                address = addressOfEachClientConected
+                userPoints[address] += POINTS_NONE_ANSWER
+                sendMessageToClient("Any correct response in 10 seconds.")
+                sendMessageToClient("Pontuaction changed: {0}. New pontuation: {1}".format(POINTS_NONE_ANSWER, userPoints[address]))
+            
+            waitingResponse = True
+            waitingResponseTimer = int(round(time.time() * 1000))
+            atualQuestion = runNextQuestion(atualQuestion + 1)
 
 serverThread = threading.Thread(target=serverCycle, args=[])
 serverThread.start()
+
+timerThread = threading.Thread(target=timerCycle, args=[])
+timerThread.start()
